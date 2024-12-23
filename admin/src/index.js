9f66a0f4f2e99fc00fd7258ebec1e39aa9f7c358
Thomas Rijpstra <thomas@fourlights.nl>
2024-12-23 14:05:11 +0100

    remove commented-out code

--- admin/src/index.js
+++ admin/src/index.js
@@ -1,4 +1,3 @@
-import { prefixPluginTranslations } from '@strapi/helper-plugin';
 import pluginPkg from '../../package.json';
 import pluginId from './pluginId';
 import Initializer from './components/Initializer';
@@ -8,26 +7,6 @@ const name = pluginPkg.strapi.name;
 
 export default {
   register(app) {
-    // app.addMenuLink({
-    //   to: `/plugins/${pluginId}`,
-    //   icon: PluginIcon,
-    //   intlLabel: {
-    //     id: `${pluginId}.plugin.name`,
-    //     defaultMessage: name,
-    //   },
-    //   Component: async () => {
-    //     const component = await import(/* webpackChunkName: "[request]" */ './pages/App');
-
-    //     return component;
-    //   },
-    //   permissions: [
-    //     // Uncomment to set the permissions of the plugin here
-    //     // {
-    //     //   action: '', // the action name should be plugin::plugin-name.actionType
-    //     //   subject: null,
-    //     // },
-    //   ],
-    // });
     app.registerPlugin({
       id: pluginId,
       initializer: Initializer,
